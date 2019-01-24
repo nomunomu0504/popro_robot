bdfd84e6fd6287ea746a89b41450300247ad31c8
yuuuo <yuuto_ose@yahoo.co.jp>
2019-01-24 17:53:56 +0900

    add cmd input

--- Popro_robo/Popro_robo.ino
+++ Popro_robo/Popro_robo.ino
@@ -3,7 +3,7 @@
 
 BLE ble;
 
-void Receive(){
+void Receive() {
   //Read WV Message
   Serial.println(ble.mes);
 }
@@ -12,6 +12,19 @@ void setup() {
   ble.attachReceiveComplete(Receive);
 }
 
+char buff[100];
+int i = 0;
+
 void loop() {
   ble.getData();
+  if (Serial.available()) {
+    buff[i] = Serial.read();
+    if (buff[i] == '\n') {
+      buff[i] = '\0';
+      i = 0;
+      ble.setData("12345678901234567890123456789013",buff);
+    } else {
+      i++;
+    }
+  }
 }
