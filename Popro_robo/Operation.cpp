9207328f2075709d3a75013692280c028f7a4d2d
yuuu <yuuto_ose@yahoo.co.jp>
2019-01-26 15:06:53 +0900

    add Operation 命令1つを実行するclass

---
+++ Popro_robo/Operation.cpp
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+#include "pinmap.h"
+#include "Operation.h"
+
+Operation::Operation(){
+    
+}
+
+void Operation::Run(char cmd){
+    
+}
---
+++ Popro_robo/Operation.h
@@ -0,0 +1,13 @@
+#ifndef OPERATION_H
+#define OPERATION_H
+
+class Operation
+{
+public:
+  Operation();
+  void Run(char cmd);
+
+private:
+};
+
+#endif
