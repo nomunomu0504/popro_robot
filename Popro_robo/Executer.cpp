17367f1dd20aaa59aa610034e78342b71408cd72
yuuu <yuuto_ose@yahoo.co.jp>
2019-01-26 20:38:14 +0900

    add Control enum

--- Popro_robo/Executer.cpp
+++ Popro_robo/Executer.cpp
@@ -2,10 +2,23 @@
 #include "pinmap.h"
 #include "Executer.h"
 
-Executer::Executer(char code[]){
-
+Executer::Executer(char code[])
+{
 }
 
-void Executer::Run(){
-    
+void Executer::Run()
+{
+}
+
+bool Executer::isControl(char cmd)
+{
+    switch (cmd)
+    {
+    case LOOP:
+        return true;
+        break;
+    default:
+        return false;
+        break;
+    }
 }
--- Popro_robo/Executer.h
+++ Popro_robo/Executer.h
@@ -1,6 +1,10 @@
 #ifndef EXECUTER_H
 #define EXECUTER_H
 
+enum Control {
+  LOOP = 'W'
+};
+
 class Executer
 {
 public:
@@ -8,6 +12,7 @@ public:
   void Run();
 
 private:
+  bool isControl(char cmd);
 };
 
 #endif
